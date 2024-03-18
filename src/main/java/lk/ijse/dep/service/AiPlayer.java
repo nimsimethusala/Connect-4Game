09132b29ca4947b669c63e6c9dc228b5511f5c1c
nimsimethusala <nimsimethusala42@gmail.com>
2024-03-18 19:37:27 +0530

    commit

--- src/main/java/lk/ijse/dep/service/AiPlayer.java
+++ src/main/java/lk/ijse/dep/service/AiPlayer.java
@@ -1,94 +1,67 @@
 package lk.ijse.dep.service;
 
+import java.util.Random;
+
 public class AiPlayer extends Player {
-    public int contt;
-    public AiPlayer(Board board) {
-        super(board);
+    public AiPlayer(Board newBoard) {
+        super(newBoard);
     }
 
+    @Override
     public void movePiece(int col) {
-        col = colChoser();
+        int x = colChosser();
+        if (x== -1){
+            do {
+                //  Random rand = new Random();
+                //  col = rand.nextInt(6);
+                int range = 6;
+                col = (int)(Math.random()*range);
+
+            }while (!board.isLegalMove(col));
+        }else {
+            col = x;
+        }
+
         board.updateMove(col, Piece.GREEN);
         board.getBoardUI().update(col, false);
-        Winner winner = board.findWinner();
-        if (winner.getWinningPiece() != Piece.EMPTY) {
-            board.getBoardUI().notifyWinner(winner);
-        } else if (!board.existLegalMoves()) {
-            this.board.getBoardUI().notifyWinner(new Winner(Piece.EMPTY));
+
+        if(board.findWinner().getWinningPiece()!=(Piece.EMPTY)) {
+            board.getBoardUI().notifyWinner(board.findWinner());
+        }else {
+            if (!board.existLegalMoves()) board.getBoardUI().notifyWinner(new Winner(Piece.EMPTY));
         }
     }
 
-    private int colChoser() {
-        int tiedColumn = 0;
-        boolean UserWinningStats = false;
-        for (int i = 0; i < 6; i++) {
-            if (this.board.isLegalMove(i) && board.existLegalMoves()) {
-                int row = this.board.findNextAvailableSpot(i);
-                this.board.updateMove(i, Piece.GREEN);
-                int eval = minimax(0, false);
-                this.board.updateMove(i, row, Piece.EMPTY);
-                if (eval == 1) {
-                    contt = 0;
+    private int colChosser() {
+        for (int i = 0; i <6; i++) {
+            if (board.isLegalMove(i) ){
+                int row = board.findNextAvailableSpot(i);
+                board.updateMove(i,Piece.GREEN);
+                if (board.findWinner().getWinningPiece() == Piece.GREEN) {
+
+                    board.updateMove(i, row, Piece.EMPTY);
                     return i;
                 }
-                if (eval == -1) {
-                    UserWinningStats = true;
-                }else {
-                    tiedColumn = i;
+                else{
+                    board.updateMove(i, row, Piece.EMPTY);
                 }
             }
         }
-        if ((UserWinningStats) && (this.board.isLegalMove(tiedColumn))) {
-            contt = 0;
-            return tiedColumn;
-        }
-        int col = 0;
-        do {
-            col = (int) (Math.random() * 6);
-        }while (!this.board.isLegalMove(col));
-        contt = 0;
-        return col;
-    }
 
-    private int minimax(int depth,  boolean maximizingPlayer) {
-        contt++;
-        Winner winner = this.board.findWinner();
-        if (winner.getWinningPiece() == Piece.GREEN) {
-            return 1;
-        }
-        if (winner.getWinningPiece() == Piece.BLUE) {
-            return -1;
-        }
-        if ((!this.board.existLegalMoves()) || (depth == 2)) {
-            return 0;
-        }
-        if (this.board.existLegalMoves()) {
-            if (maximizingPlayer) {
-                for (int i = 0; i < 6; i++)
-                    if (this.board.isLegalMove(i)) {
-                        int row = this.board.findNextAvailableSpot(i);
-                        this.board.updateMove(i, Piece.GREEN);
-                        int eval = minimax(depth + 1,false);
-                        this.board.updateMove(i, row, Piece.EMPTY);
-                        if (eval == 1) {
-                            return eval;
+        for (int i = 0; i <6; i++) {
+            if (board.isLegalMove(i) ){
+                int row = board.findNextAvailableSpot(i);
+                board.updateMove(i,Piece.BLUE);
+                if (board.findWinner().getWinningPiece() == Piece.BLUE) {
 
-                        }
-                    }
-            } else {
-                for (int i = 0; i < 6; i++) {
-                    if (this.board.isLegalMove(i)) {
-                        int row = this.board.findNextAvailableSpot(i);
-                        this.board.updateMove(i, Piece.BLUE);
-                        int eval = minimax(depth + 1, true);
-                        this.board.updateMove(i, row, Piece.EMPTY);
-                        if (eval == -1) {
-                            return eval;
-                        }
-                    }
+                    board.updateMove(i, row, Piece.EMPTY);
+                    return i;
+                }
+                else{
+                    board.updateMove(i, row, Piece.EMPTY);
                 }
             }
         }
-        return 0;
+        return -1;
     }
 }
